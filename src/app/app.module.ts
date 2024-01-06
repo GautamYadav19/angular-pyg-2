b227f45eebfd07660ef800d712c8c631f353b238
Gautam Yadav <gautamyadav1992002@gmail.com>
2024-01-06 15:37:56 +0530

    configure first script and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { FeaturedComponent } from './components/featured/featured.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { OffersComponent } from './components/offers/offers.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { VendorComponent } from './components/vendor/vendor.component';
+import { SingleProductComponent } from './components/products/single-product/single-product.component';
+import { SubscribeComponent } from './components/subscribe/subscribe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CategoriesComponent,
+    FeaturedComponent,
+    FooterComponent,
+    NavbarComponent,
+    OffersComponent,
+    TopbarComponent,
+    VendorComponent,
+    SingleProductComponent,
+    SubscribeComponent
   ],
   imports: [
     BrowserModule,
